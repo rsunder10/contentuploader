b46163401a2479e15098c567c10ff71e8828bb35
rsunder10 <sunderis10@gmail.com>
2015-09-21 20:34:05 +0530

    Initial Start

---
+++ index.php
@@ -0,0 +1,14 @@
+<form role="form" method="post" action="" autocomplete="off">
+
+	<h2>Please Sign Up</h2>
+	<p>Already a member? <a href='login.php'>Login</a></p>
+	<hr>
+
+	<input id="firstName" type="text" name="firstName">
+	<input id="lastName" type="text" name="lastName">
+	<input id="regNo" type="text" name="regNo">
+	<input id="domain" type="text" name="domain">
+	<input id="username" type="email" name="email">
+	<input id="password" type="password" name="password">
+
+</form>
---
+++ layout/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title><?php if(isset($title)){ echo $title; }?></title>
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
