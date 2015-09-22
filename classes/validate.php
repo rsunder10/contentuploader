82e73b91ff48638ac4192d77a1d464ba39dc1c99
rsunder10 <sunderis10@gmail.com>
2015-09-22 19:13:32 +0530

    Modification Done

---
+++ classes/validate.php
@@ -0,0 +1,119 @@
+<?php
+class Validate{
+
+function valid_email($email)
+{
+    if (!ereg("^[^@]{1,64}@[^@]{1,255}$", $email))
+    {
+      
+        return false;
+    }
+    $email_array = explode("@", $email);
+    $local_array = explode(".", $email_array[0]);
+    for ($i = 0; $i < sizeof($local_array); $i++)
+    {
+        if (!ereg("^(([A-Za-z0-9!#$%&#038;'*+/=?^_`{|}~-][A-Za-z0-9!#$%&#038;'*+/=?^_`{|}~\.-]{0,63})|(\"[^(\\|\")]{0,62}\"))$",
+            $local_array[$i]))
+        {
+            return false;
+        }
+    }
+    if (!ereg("^\[?[0-9\.]+\]?$", $email_array[1]))
+    { 
+        $domain_array = explode(".", $email_array[1]);
+        if (sizeof($domain_array) < 2)
+        {
+            return false;
+        }
+        for ($i = 0; $i < sizeof($domain_array); $i++)
+        {
+            if (!ereg("^(([A-Za-z0-9][A-Za-z0-9-]{0,61}[A-Za-z0-9])|([A-Za-z0-9]+))$", $domain_array[$i]))
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+
+function valid_password($pass,$pass2,$minlength = 7, $maxlength = 100)
+{
+    $pass = trim($pass);
+ 
+    if (empty($pass))
+    {
+        return false;
+    }
+ 
+    if (strlen($pass) < $minlength)
+    {
+        return false;
+    }
+ 
+    if (strlen($pass) > $maxlength)
+    {
+        return false;
+    }
+	$pass2=trim($pass2);
+	if ($pass!=$pass2){
+	
+	return false;
+	
+	}
+	return true;
+}
+
+
+
+function valid_username_name($name,$minlength = 7, $maxlength = 100)
+{
+    $name = trim($name);
+ 
+    if (empty($name))
+    {
+        return false;
+    }
+ 
+    if (strlen($name) < $minlength)
+    {
+        return false;
+    }
+ 
+    if (strlen($name) > $maxlength)
+    {
+        return false;
+    }
+
+	return true;
+}
+
+function valid_register($registerno, $minlength = 8, $maxlength = 25)
+{
+ 
+    $registerno = trim($registerno);
+ 
+    if (empty($registerno))
+    {
+        return false; 
+    }
+    if (strlen($registerno) > $maxlength)
+    {
+        return false; 
+    }
+    if (strlen($registerno) < $minlength)
+    {
+ 
+        return false; 
+    }
+ 	
+
+ return true;
+}
+
+}
+
+
+
+
+?>
--- index.php
+++ index.php
@@ -3,12 +3,34 @@ require('classes/db.php');
 $db=new database();
 $db->connect();
 ?>
-<form role="form" method="post" action="" autocomplete="off">
+
+<?php 
+	require('layout/header.php')
+?>
+<form role="form" method="get" action="#" autocomplete="off">
 
 	<h2>Please Sign Up</h2>
 	<p>Already a member? <a href='login.php'>Login</a></p>
 	<hr>
 
+	<div>
+		<input id="username" type="text" name="user" placeholder="username">
+	</div>
+	<br/>
+
+	<div>
+		<input id="email" type="email" name="email" placeholder="email">
+	</div>
+	<br/>
+	<div>
+		<input id="password1" type="password" name="password1" placeholder="password">
+	</div>
+	<br/>
+	<div>
+		<input id="password2" type="password" name="password2" placeholder="confirm password">
+	</div>
+	<br/>
+	
 	<div>
 		<input id="firstName" type="text" name="firstName" placeholder="firstname">
 	</div>
@@ -22,15 +44,18 @@ $db->connect();
 	</div>
 	<br/>
 	<div>
+		<input id="contact" type="tel" name="contact" placeholder="contact">
+	</div>
+	<br/>
+	<div>
 		<input id="domain" type="text" name="domain" placeholder="Domain">
 	</div>
 	<br/>
-	<div>
-		<input id="username" type="email" name="email" placeholder="username">
-	</div>
-	<br/>
-	<div>
-		<input id="password" type="password" name="password" placeholder="password">
-	</div>
-	<br/>
+
+<input type="submit" value="SignUp" id ="submit">
+
 </form>
+
+<?php 
+	require('layout/footer.php')
+?>
