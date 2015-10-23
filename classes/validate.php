463abf82ab85347dc6fabf3d574e486a40cdc135
rsunder10 <sunderis10@gmail.com>
2015-10-23 14:51:10 +0530

    Making PHP 5.5 compatability

--- classes/validate.php
+++ classes/validate.php
@@ -10,7 +10,7 @@ function test_input($data) {
 
 function valid_email($email)
 {
-    if (!eregi("^[^@]{1,64}@[^@]{1,255}$", $email))
+    if (!preg_match("^[^@]{1,64}@[^@]{1,255}$", $email))
     {
       
         return false;
@@ -19,13 +19,13 @@ function valid_email($email)
     $local_array = explode(".", $email_array[0]);
     for ($i = 0; $i < sizeof($local_array); $i++)
     {
-        if (!eregi("^(([A-Za-z0-9!#$%&#038;'*+/=?^_`{|}~-][A-Za-z0-9!#$%&#038;'*+/=?^_`{|}~\.-]{0,63})|(\"[^(\\|\")]{0,62}\"))$",
+        if (!preg_match("^(([A-Za-z0-9!#$%&#038;'*+/=?^_`{|}~-][A-Za-z0-9!#$%&#038;'*+/=?^_`{|}~\.-]{0,63})|(\"[^(\\|\")]{0,62}\"))$",
             $local_array[$i]))
         {
             return false;
         }
     }
-    if (!eregi("^\[?[0-9\.]+\]?$", $email_array[1]))
+    if (!preg_match("^\[?[0-9\.]+\]?$", $email_array[1]))
     { 
         $domain_array = explode(".", $email_array[1]);
         if (sizeof($domain_array) < 2)
@@ -34,7 +34,7 @@ function valid_email($email)
         }
         for ($i = 0; $i < sizeof($domain_array); $i++)
         {
-            if (!eregi("^(([A-Za-z0-9][A-Za-z0-9-]{0,61}[A-Za-z0-9])|([A-Za-z0-9]+))$", $domain_array[$i]))
+            if (!preg_match("^(([A-Za-z0-9][A-Za-z0-9-]{0,61}[A-Za-z0-9])|([A-Za-z0-9]+))$", $domain_array[$i]))
             {
                 return false;
             }
